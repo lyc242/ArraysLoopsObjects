0b6f05f2fc0930187992e0071167469be14b3ba7
LYC <lyc242@qq.com>
2019-02-23 00:40:50 +0800

    update

--- js/student_report.js
+++ js/student_report.js
@@ -1,21 +1,39 @@
+var student;
+var search;
+var html = "";
+
 function print(message) {
     var outputDiv = document.getElementById('output');
     outputDiv.innerHTML = message;
   }
 
-
-var html ="";
-var student;
-
-
-for (var i = 0;i < students.length; i+=1 ){
-    student = students[i];
-    html += "<h2>Student: "+student.Name+"</h2>";
-    html += "<p>Track: " +student.Track+"</p>";
-    html += "<p>Achievements: "+student.Achievements+"</p>";
-    html += "<p>Points: "+student.Points+"</p>";
+function getReport(student) {
+    var report = "<h2>Student: "+student.Name+"</h2>";
+    report += "<p>Track: " +student.Track+"</p>";
+    report += "<p>Achievements: "+student.Achievements+"</p>";
+    report += "<p>Points: "+student.Points+"</p>";
+    return report;
 }
 
 
 
-print(html);
+while (true) {
+    search = prompt("Please enter the name,Enter 'Q' to quit");
+    if (search.toLowerCase()=== "q" || search === null){
+        break;
+    }
+    for (var i = 0;i < students.length; i+=1 ){
+        student = students[i];
+        if (student.Name.toLowerCase() === search.toLowerCase()) {
+            html = getReport(student);
+            print(html);
+        }
+    }
+    break;
+
+}
+
+
+
+
+
